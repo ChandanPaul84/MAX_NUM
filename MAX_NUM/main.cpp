7b16820537fab5e86935cbba8490caa03d519c1d
ChandanPaul84 <chandan.kr.paul84@gmail.com>
2015-11-26 23:18:34 +0530

    next Greater number
    Next Greater Number

--- MAX_NUM/main.cpp
+++ MAX_NUM/main.cpp
@@ -7,9 +7,49 @@
 //
 
 #include <iostream>
+#include <string.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void returnNextMaxNum(char arr[]);
+void reverse(char *arr,int i, int len);
+
+void returnNextMaxNum(char arr[]){
+    int len = (int)strlen(arr);
+    int i,j;
+    for(i= len-1; i>0; i--){
+        if(arr[i] < arr[i-1]){
+            continue;
+        }
+        break;
+    }
+    for(j =i; j< len-1; j++){
+        if(arr[i-1] < arr[j]){
+            continue;
+        }
+        break;
+    }
+    if(i>1){
+        char temp = arr[i-1];
+        arr[i-1] = arr[j];
+        arr[j] = temp;
+    }
+    reverse(arr, i, len);
+}
+
+void reverse(char *arr, int i, int len)
+{
+    int start = i; int end = len-1;
+    while( start < end){
+        char temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end --;
+    }
+}
+
+int main() {
+    char arr[] = "218765";
+    returnNextMaxNum(arr);
+    puts(arr);
     return 0;
 }
